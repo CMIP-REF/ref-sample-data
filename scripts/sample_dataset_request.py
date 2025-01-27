d169f42dedd8bc1f955b6c5b05d1de894e0803ae
Dora Hegedus <dora.hegedus@stfc.ac.uk>
2025-01-27 14:34:26 +0000

    added obs4mips data request class

--- scripts/sample_dataset_request.py
+++ scripts/sample_dataset_request.py
@@ -30,7 +30,7 @@ class DataRequest(ABC):
         """Downscale the dataset to a smaller size."""
         pass
 
-    def create_out_filename(self, metadata: pd.Series, ds: xr.Dataset) -> pathlib.Path:
+    def create_out_filename(self, metadata: pd.Series, ds: xr.Dataset, ds_filename: str) -> pathlib.Path:
         """Create the output filename for the dataset."""
         pass
 
@@ -123,7 +123,7 @@ class CMIP6Request(DataRequest):
 
         return result
 
-    def create_out_filename(self, metadata: pd.Series, ds: xr.Dataset) -> pathlib.Path:
+    def create_out_filename(self, metadata: pd.Series, ds: xr.Dataset, ds_filename: str) -> pathlib.Path:
         """
         Create the output filename for the dataset.
 
@@ -141,6 +141,120 @@ class CMIP6Request(DataRequest):
             / f"v{metadata['version']}"
         )
         filename_prefix = "_".join([metadata[item] for item in self.cmip6_filename_paths])
+
+        if "time" in ds.dims:
+            time_range = (
+                f"{ds.time.min().dt.strftime('%Y%m').item()}-{ds.time.max().dt.strftime('%Y%m').item()}"
+            )
+            filename = f"{filename_prefix}_{time_range}.nc"
+        else:
+            filename = f"{filename_prefix}.nc"
+
+        return output_path / filename
+
+
+class obs4MIPsRequest(DataRequest):
+    """
+    Represents a Obs4MIPs dataset request
+
+    """
+
+    def __init__(self, facets: dict[str, Any], remove_ensembles: bool, time_span: tuple[str, str] | None):
+        self.avail_facets = [
+            "activity_id",
+            "institution_id",
+            "source_id",
+            "frequency",
+            "variable_id",
+            "grid_label",
+            "version",
+            "data_node",
+        ]
+
+        self.facets = facets
+
+        self.remove_ensembles = remove_ensembles
+        self.time_span = time_span
+
+        self.obs4mips_path_items = [
+            "activity_id",
+            "institution_id",
+            "source_id",
+            "variable_id",
+            "grid_label",
+        ]
+
+        self.obs4mips_filename_paths = [
+            "variable_id",
+            "source_id",
+            "grid_label",
+        ]
+
+        assert all(key in self.avail_facets for key in self.obs4mips_path_items), "Error message"
+        assert all(key in self.avail_facets for key in self.obs4mips_filename_paths), "Error message"
+
+    def decimate_dataset(self, dataset: xr.Dataset, time_span: tuple[str, str] | None) -> xr.Dataset | None:
+        """
+        Downscale the dataset to a smaller size.
+
+        Parameters
+        ----------
+        dataset
+            The dataset to downscale
+        time_span
+            The time span to extract from a dataset
+
+        Returns
+        -------
+        xr.Dataset
+            The downscaled dataset
+        """
+        has_latlon = "lat" in dataset.dims and "lon" in dataset.dims
+        has_ij = "i" in dataset.dims and "j" in dataset.dims
+
+        if has_latlon:
+            assert len(dataset.lat.dims) == 1 and len(dataset.lon.dims) == 1
+            result = dataset.interp(lat=dataset.lat[:10], lon=dataset.lon[:10])
+        elif has_ij:
+            # 2d lat/lon grid (generally ocean variables)
+            # Choose a starting point around the middle of the grid to maximise chance that it has values
+            # TODO: Be smarter about this?
+            j_midpoint = len(dataset.j) // 2
+            result = dataset.interp(i=dataset.i[:10], j=dataset.j[j_midpoint : j_midpoint + 10])
+        else:
+            raise ValueError("Cannot decimate this grid: too many dimensions")
+
+        if "time" in dataset.dims and time_span is not None:
+            result = result.sel(time=slice(*time_span))
+            if result.time.size == 0:
+                result = None
+
+        return result
+
+    def create_out_filename(self, metadata: pd.Series, ds: xr.Dataset, ds_filename: str) -> pathlib.Path:
+        """
+        Create the output filename for the dataset.
+
+        Parameters
+        ----------
+        ds
+            Loaded dataset
+
+        Returns
+        -------
+            The output filename
+        """
+        output_path = (
+            Path(os.path.join(*[metadata[item] for item in self.obs4mips_path_items]))
+            / f"v{metadata['version']}"
+        )
+        if ds_filename.name.split("_")[0] == ds.variable_id:
+            filename_prefix = "_".join([metadata[item] for item in self.obs4mips_filename_paths])
+        else:
+            filename_prefix = ds_filename.name.split("_")[0] + "_"
+            filename_prefix += "_".join(
+                [metadata[item] for item in self.obs4mips_filename_paths if item != "variable_id"]
+            )
 
         if "time" in ds.dims:
             time_range = (
@@ -234,7 +348,9 @@ def create_sample_dataset(DataRequest):
             if ds_decimated is None:
                 continue
 
-            output_filename = OUTPUT_PATH / DataRequest.create_out_filename(dataset, ds_decimated)
+            output_filename = OUTPUT_PATH / DataRequest.create_out_filename(
+                dataset, ds_decimated, ds_filename
+            )
             output_filename.parent.mkdir(parents=True, exist_ok=True)
             ds_decimated.to_netcdf(output_filename)
             print(output_filename)
@@ -244,52 +360,55 @@ def create_sample_dataset(DataRequest):
 
 
 if __name__ == "__main__":
-    facets_to_fetch = [
+    datasets_to_fetch = [
         # Example metric data
-        dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "tas", "tos", "rsut", "rlut", "rsdt"],
-            experiment_id=["ssp126", "historical"],
+        CMIP6Request(
+            facets=dict(
+                source_id="ACCESS-ESM1-5",
+                frequency=["fx", "mon"],
+                variable_id=["areacella", "tas", "tos", "rsut", "rlut", "rsdt"],
+                experiment_id=["ssp126", "historical"],
+            ),
             remove_ensembles=True,
             time_span=("2000", "2025"),
         ),
         # ESMValTool ECS data
-        dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "rlut", "rsdt", "rsut", "tas"],
-            experiment_id=["abrupt-4xCO2", "piControl"],
+        CMIP6Request(
+            facets=dict(
+                source_id="ACCESS-ESM1-5",
+                frequency=["fx", "mon"],
+                variable_id=["areacella", "rlut", "rsdt", "rsut", "tas"],
+                experiment_id=["abrupt-4xCO2", "piControl"],
+            ),
             remove_ensembles=True,
             time_span=("0101", "0125"),
         ),
         # ESMValTool TCR data
-        dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "tas"],
-            experiment_id=["1pctCO2", "piControl"],
+        CMIP6Request(
+            facets=dict(
+                source_id="ACCESS-ESM1-5",
+                frequency=["fx", "mon"],
+                variable_id=["areacella", "tas"],
+                experiment_id=["1pctCO2", "piControl"],
+            ),
             remove_ensembles=True,
             time_span=("0101", "0180"),
         ),
         # Obs4MIPs AIRS data
-        # dict(
-        #    project="obs4MIPs",
-        #    institution_id="NASA-JPL",
-        #    frequency="mon",
-        #    source_id="AIRS-2-1",
-        #    variable_id="ta",
-        #    remove_ensembles=False,
-        #    time_span=("2002", "2016"),
-        # ),
+        obs4MIPsRequest(
+            facets=dict(
+                project="obs4MIPs",
+                institution_id="NASA-JPL",
+                frequency="mon",
+                source_id="AIRS-2-1",
+                variable_id="ta",
+            ),
+            remove_ensembles=False,
+            time_span=("2002", "2016"),
+        ),
     ]
 
     facet_id_keys = ["source_id", "frequency", "variable_id", "experiment_id"]
 
-    for facets in facets_to_fetch:
-        CMIP6DataRequest = CMIP6Request(
-            facets={x: facets[x] for x in facet_id_keys},
-            remove_ensembles=facets["remove_ensembles"],
-            time_span=facets["time_span"],
-        )
-        create_sample_dataset(CMIP6DataRequest)
+    for dataset_requested in datasets_to_fetch:
+        create_sample_dataset(dataset_requested)
